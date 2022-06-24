a66650f58f8ba27b61a56c4a5dc5c32ae57f70b1
Mehmet Bedir SEVIMLI <mehmetbedirsevimli123@gmail.com>
2022-06-24 04:01:22 +0300

    table listed

--- react-setup/src/App.js
+++ react-setup/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
+import axios from "axios";
 
-function App() {
+export default function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    axios
+    .get("https://restcountries.com/v2/all")
+    .then(response => setCountries(response.data))
+    .catch(error => console.log({ error}));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React</h1>
+      {countries.map(country => {
+      return (
+      <div key={country.name}>
+        <table className="table table-striped table-dark">
+  <thead>
+    <tr>
+      <th scope="col">name</th>
+      <th scope="col">capital</th>
+      <th scope="col">region</th>
+      <th scope="col">flag</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">{country.name}</th>
+      <td>{country.capital}</td>
+      <td>{country.region}</td>
+      <td><img src={country.flag} class="img-fluid" alt={country.name}></img></td>
+    </tr>
+  </tbody>
+</table>
+      </div>
+      ); 
+      })}
     </div>
   );
 }
 
-export default App;
